353c26751628a581426a91a8fc5deb8e8d0afbb2
faizan517 <faizanpervaiz517@gmail.com>
2019-08-29 19:52:11 +0500

    js assignment1

---
+++ js assignment_1/js/app.js
@@ -0,0 +1,92 @@
+//alert;
+alert('first js assignment ');
+window.alert("hello");
+
+
+//variable of string;
+var message = 'Hello World';
+alert(message);
+var email = "fazi@gmail.com";
+alert(email);
+var lastMessage;
+lastMessage = 'bye';
+console.log (lastMessage);
+
+
+//variable of number;
+var num = 20;
+console.log (num);
+var num =20.24;
+var _pi =3.336;
+console.log (_pi);
+var $number = 0.10
+console.log($number);
+console.log (num);
+var two = 6 - 5;
+console.log (two);
+var three ="6"+ 5;
+console.log (three);
+
+
+//Familiar operators
+var two = 6 - 5;
+console.log (two);
+var  numbers = 4;
+console.log (numbers);
+alert(2+2);
+var popularNumber = 12 - 24;
+console.log(popularNumber);
+var popularNumber = 12 * 24;
+console.log(popularNumber);
+var num = 10; 
+var popularNumber = num + 200;
+console.log(popularNumber)
+var num = 10; 
+var anotherNum = 1; 
+var popularNumber = num + anotherNum;
+console.log(popularNumber);
+
+
+//Unfamiliar operators
+var unfamiliar=5;
+console.log(unfamiliar++);
+console.log(unfamiliar);
+
+var unfamiliars=8;
+console.log(unfamiliars--);
+console.log(unfamiliars);
+
+var unfamiliars=9;
+console.log(++unfamiliars);
+console.log(++unfamiliars);
+
+var unfamiliars=9;
+console.log(--unfamiliars);
+
+
+//math expressions
+var important =5*2-2;
+console.log(important);
+var total= 1 + (3*4);
+console.log(total);
+var resultOfComputation = (2 * 4) * 4 + 2;
+console.log(resultOfComputation);
+
+
+//Concatenation
+var new1 = "my name";
+var new2 = 'is faizan';
+console.log(new1 + new2);
+var new1 = "my name";
+var new2 = ' is';
+document.write(new1 + new2 +" "+ "faizan");
+
+var str1 = 'Hello';
+var str2 ="Friends";
+var new_str =str1.concat (str2);
+console.log(new_str);
+var str1 = ' Hello';
+var str2 =" Friends";
+var new_str =str1.concat (" my", str2);
+console.log(new_str);
+
